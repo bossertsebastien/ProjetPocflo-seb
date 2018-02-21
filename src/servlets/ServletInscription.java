5f1134d9773644b56c0e91c52f316d7a5b3878ce
kerherfl <kerherfl@15de052.univ-brest.fr>
2018-02-21 17:38:13 +0100

    redirection accueil

--- src/servlets/ServletInscription.java
+++ src/servlets/ServletInscription.java
@@ -54,7 +54,7 @@ public class ServletInscription extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		request.getServletContext().getRequestDispatcher("/html/accueil.jsp").forward(request, response);
 	}
 
 }
